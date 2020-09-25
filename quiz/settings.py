c27997c940d90c11a6bfd87012fc7767c00518ed
rohitgeddam <rohitgeddam2018@gmail.com>
2020-09-25 09:25:07 +0530

    modified settings.py to use static files. created static folder at root.

--- quiz/settings.py
+++ quiz/settings.py
@@ -125,3 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'static'),]
+
+# static_root is for production
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
